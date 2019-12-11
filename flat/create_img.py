314ce56cf436ab1d4f86e616189be8be2d048260
daikonbu <manichado@icloud.com>
2019-12-11 15:07:57 +0900

    a

--- flat/create_img.py
+++ flat/create_img.py
@@ -1,16 +1,16 @@
 from PIL import Image, ImageDraw, ImageFont
 def create_img(theme,id):
-
+    font = ImageFont.truetype("../.font/ipaexm.ttf", 27)
 
     img = Image.open("media/frame.jpg")
     W,H=img.size
 
     draw = ImageDraw.Draw(img)
-    w,h=draw.textsize(theme)
+    w,h=draw.textsize(theme,font=font)
 
+    #フォントの設定(フォントファイルのパスと文字の大きさ)
 
-
-    draw.text(((W-w)/2,(H-h)/2), theme, fill=(0, 0, 255))
+    draw.text(((W-w)/2,(H-h)/2), theme, fill=(0, 0, 255), font=font)
     img.save("media/img_"+str(id)+".jpg")
 
 
