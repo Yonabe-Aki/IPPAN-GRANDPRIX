01e2ac41e0735f901369db14c464d9aa0187a28f
daikonbu <manichado@icloud.com>
2019-12-13 19:22:16 +0900

    A

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,8 @@ from django.contrib.auth import views as auth_views
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 
 
@@ -15,7 +17,8 @@ urlpatterns = [
     path("",include("flat.urls")),
     path('', include('social_django.urls', namespace='social')),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
