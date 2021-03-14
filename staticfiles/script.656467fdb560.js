559dc632663920ad653dcbe86950e483731119b1
manimani <manichado@icloud.com>
2021-03-14 16:41:00 +0900

    staticfileの削除

--- staticfiles/script.656467fdb560.js
+++
@@ -1,6 +0,0 @@
-function show_modal_window(id){
-    document.getElementById("modal_window"+String(id)).style.display="flex";
-}
-function close_modal_window(id){
-    document.getElementById("modal_window"+String(id)).style.display="none";
-}
