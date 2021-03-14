58042c01ed4d2a86b495fc5228bf05769f342c4f
manimani <manichado@icloud.com>
2021-03-14 16:38:09 +0900

    cssファイルにcharset追加

---
+++ staticfiles/script.js
@@ -0,0 +1,6 @@
+function show_modal_window(id){
+    document.getElementById("modal_window"+String(id)).style.display="flex";
+}
+function close_modal_window(id){
+    document.getElementById("modal_window"+String(id)).style.display="none";
+}
