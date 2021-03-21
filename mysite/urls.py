b9545e5c8636528ecb30f960f2583a29acf285e2
manimani <manichado@icloud.com>
2021-03-21 20:08:00 +0900

    urlpatternにSTATIC_URLとSTATIC_ROOTの紐付けの設定を追加

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from django.urls import path,include
 from flat import views
 from django.contrib.auth import views as auth_views
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import re_path
@@ -16,5 +15,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
