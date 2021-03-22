2adea1ab1a08ef0c92e9247b5a922f9eff45476b
manimani <manichado@icloud.com>
2021-03-22 12:41:17 +0900

    setting.pyのSTATiCFILES_STORAGEをコメントアウト

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
