c797b48a4667cb3cfb934432ebf1300e85ad4f48
manimani <manichado@icloud.com>
2021-03-24 04:25:28 +0900

    a

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 # mode="ローカルテスト"
 # mode="ローカルテスト(DEBUG=False)"
-# mode="デプロイテスト"
-mode="本番"
+mode="デプロイテスト"
+# mode="本番"
 
 import os
 import django_heroku
