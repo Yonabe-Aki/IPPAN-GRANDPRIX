8c2d3ec2191000ef41b1f6d2a9d44a0532f59a46
manimani <manichado@icloud.com>
2021-05-16 18:44:05 +0900

    javascript周りのエラー解消

--- static/script.js
+++ static/script.js
@@ -5,21 +5,27 @@ function require_login(){
 var speed;
 var fadeOut;
 var count;
-function fadeOut(){
-    speed = speed * acceleration
-    var target = document.getElementById('flash-message');
-    var cssStyleDeclaration = getComputedStyle(target, null ) ;
-    var top = cssStyleDeclaration.getPropertyValue( "top" ) ;
-    target.style.top = parseInt(top) - parseInt(speed) +"px";   
-    if(parseInt(top) <= -300){
-        clearInterval(timer1);
-        target.remove();
-    }
-}
+var acceleration;
+
 function test2(){
     speed = 1;
     acceleration = 1.2;
 }
+
+function fadeOut(){
+    speed = speed * acceleration
+    if (document.getElementById("flash-message") != null){
+        var target = document.getElementById('flash-message');
+        var cssStyleDeclaration = getComputedStyle(target, null ) ;
+        var top = cssStyleDeclaration.getPropertyValue( "top" ) ;
+        target.style.top = parseInt(top) - parseInt(speed) +"px";   
+        if(parseInt(top) <= -300){
+            clearInterval(timer1);
+            target.remove();
+        }
+    }
+}
+
 
 timer1 = setInterval(fadeOut,50);
 
